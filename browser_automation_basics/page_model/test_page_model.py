52249ba3979baa631cd84aa79f11b2e2f8d2430c
Egor Kostan <igorkostan@gmail.com>
2019-07-25 21:30:16 -0700

    Create test_page_model.py

---
+++ browser_automation_basics/page_model/test_page_model.py
@@ -0,0 +1,26 @@
+from browser_automation_basics.locators.test_page_locator import TestPageLocator
+
+class TestPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    @property
+    def url(self):
+        '''
+        Returns web page url
+        :return:
+        '''
+        return self.driver.current_url
+
+    @property
+    def title(self):
+        '''
+        Returns <title> element
+        :return:
+        '''
+        return self.driver.title
+
+    def input_field_1(self):
+        return self.driver.find_element(*TestPageLocator.INPUT_FIELD_1)
+
