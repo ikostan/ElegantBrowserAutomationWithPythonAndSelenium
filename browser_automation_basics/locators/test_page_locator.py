5aea30f4882a15c666e33fa2acb5fa78dcf4715e
Egor Kostan <igorkostan@gmail.com>
2019-07-25 21:30:18 -0700

    Create test_page_locator.py

---
+++ browser_automation_basics/locators/test_page_locator.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.common.by import By
+
+
+class TestPageLocator:
+
+    URL = 'https://techstepacademy.com/training-ground'
+    TITLE = 'Training Ground — TechStep Academy'
+
+    PAGE_TITLE = (By.XPATH, '//*[@id="lower-logo"]/h1/a')
+    PARAGRAPH = (By.XPATH, '//*[@id="block-ec928cee802cf918d26c"]/div/p')
+
+    # Buttons
+    BUTTON_1 = (By.ID, 'b1')
+    BUTTON_2 = (By.ID, 'b2')
+    BUTTON_3 = (By.ID, 'b3')
+    BUTTON_4 = (By.ID, 'b4')
+
+    # Input Fields
+    INPUT_FIELD_1 = (By.CSS_SELECTOR, 'ipt1')
+    INPUT_FIELD_2 = (By.ID, 'ipt2')
+
+    # Dropdown list
+    DROPDOWN = (By.ID, 'sel1')
