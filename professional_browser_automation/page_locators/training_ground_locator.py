80c4032712b22ad1b3ed007b06b6479c77c7ac3c
Egor Kostan <igorkostan@gmail.com>
2019-07-26 23:40:25 -0700

    Update training_ground_locator.py

--- professional_browser_automation/page_locators/training_ground_locator.py
+++ professional_browser_automation/page_locators/training_ground_locator.py
@@ -5,5 +5,4 @@ from selenium.webdriver.common.by import By
 class TrainingGroundLocator(BasePageLocator):
 
 	URL = BasePageLocator.URL + '/training-ground'
-	TITLE_CONTEXT = 'Training Ground — ' + BasePageLocator.TITLE_CONTEXT
 
