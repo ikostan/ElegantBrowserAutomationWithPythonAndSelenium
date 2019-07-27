633f180c084538e250045e4fbb518f962c67deb8
Egor Kostan <igorkostan@gmail.com>
2019-07-26 23:29:36 -0700

    Update base_page_locator.py

--- professional_browser_automation/locators/base_page_locator.py
+++ professional_browser_automation/locators/base_page_locator.py
@@ -2,9 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocator:
-	# Main url
-	URL = 'https://techstepacademy.com'
 
-	# Main title
 	TITLE = (By.TAG_NAME, 'title')
-	TITLE_CONTEXT = 'TechStep Academy'
+	LOGO = (By.XPATH, '//*[@id="lower-logo"]/h1/a')
