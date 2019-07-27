eb330a89b377ab5044a35cb0f168356da3660211
Egor Kostan <igorkostan@gmail.com>
2019-07-27 13:47:58 -0700

    Update training_ground_locator.py

--- professional_browser_automation/page_locators/training_ground_locator.py
+++ professional_browser_automation/page_locators/training_ground_locator.py
@@ -4,5 +4,15 @@ from selenium.webdriver.common.by import By
 
 class TrainingGroundLocator(BasePageLocator):
 
-	URL = BasePageLocator.URL + '/training-ground'
+	# Input fields:
+	INPUT_1 = (By.ID, 'ipt1')
+	INPUT_2 = (By.ID, 'ipt2')
 
+	# Buttons:
+	Button_1 = (By.ID, 'b1')
+	Button_2 = (By.ID, 'b2')
+	Button_3 = (By.ID, 'b3')
+	Button_4 = (By.ID, 'b4')
+
+	# Drop down:
+	DROP_DOWN = (By.ID, 'sel1')
