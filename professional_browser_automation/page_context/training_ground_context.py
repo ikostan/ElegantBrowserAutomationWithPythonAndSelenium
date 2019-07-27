d238920f9f6fb1a1c67ba1cf25977f2b98dcd11b
Egor Kostan <igorkostan@gmail.com>
2019-07-26 23:40:48 -0700

    Create training_ground_context.py

---
+++ professional_browser_automation/page_context/training_ground_context.py
@@ -0,0 +1,6 @@
+from professional_browser_automation.page_context.base_page_context import BasePageContext
+
+
+class TrainingGroundContext(BasePageContext):
+	TITLE = 'Training Ground — ' + BasePageContext.TITLE
+
