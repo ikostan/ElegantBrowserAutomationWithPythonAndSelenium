bb2c63e34b58cbe32fb0322784bd12097a01af77
Egor Kostan <igorkostan@gmail.com>
2019-07-27 14:58:44 -0700

    Update training_ground_page_test.py

--- professional_browser_automation/tests/training_ground_page_test.py
+++ professional_browser_automation/tests/training_ground_page_test.py
@@ -6,17 +6,31 @@ import selenium
 
 class TrainingGroundTestCase(unittest.TestCase):
 
-	def setUp(self):
-		self.page = TrainingGroundModel(selenium.webdriver.Chrome())
-		self.page.go()
+	@classmethod
+	def setUpClass(cls):
+		cls.page = TrainingGroundModel(selenium.webdriver.Chrome())
+		cls.page.go()
 
-	def test_basics(self):
-
+	def test_url(self):
 		self.assertEqual(TrainingGroundContext.URL, self.page.url)
+
+	def test_title(self):
 		self.assertEqual(TrainingGroundContext.TITLE, self.page.title)
 
-	def tearDown(self):
-		self.page.quit()
+	def test_input_field_1(self):
+		expected_text = 'something here'
+		self.page.type_into_input_field_1(expected_text)
+		self.assertEqual(expected_text, self.page.get_input_field_1_text)
+
+	def test_alert_1(self):
+		expected_alert_text = 'You clickedButton1.'
+		self.page.click_button_1()
+		self.assertEqual(expected_alert_text, self.page.alert_1_text)
+		self.page.accept_alert_1()
+
+	@classmethod
+	def tearDownClass(cls):
+		cls.page.quit()
 
 
 if __name__ == '__main__':
