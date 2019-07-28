b595932a24b4aed07f92bad4668a7aa31d7ff408
Egor Kostan <igorkostan@gmail.com>
2019-07-28 13:07:25 -0700

    Update training_ground_context.py

--- professional_browser_automation/page_context/training_ground_context.py
+++ professional_browser_automation/page_context/training_ground_context.py
@@ -2,7 +2,9 @@ from professional_browser_automation.page_context.base_page_context import BaseP
 
 
 class TrainingGroundContext(BasePageContext):
+
 	TITLE = 'Training Ground — ' + BasePageContext.TITLE
 	URL = BasePageContext.URL + '/training-ground'
-
+	BUTTON_1 = "Button1"
+	ALERT_1 = 'You clickedButton1.'
 
