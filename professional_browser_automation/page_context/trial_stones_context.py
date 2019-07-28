f14fcffcfc178af50c5c852c2dae6e935a64c96f
Egor Kostan <igorkostan@gmail.com>
2019-07-28 16:58:12 -0700

    Create trial_stones_context.py

---
+++ professional_browser_automation/page_context/trial_stones_context.py
@@ -0,0 +1,15 @@
+from professional_browser_automation.page_context.base_page_context import BasePageContext
+
+
+class TrialStonesContext(BasePageContext):
+
+	TITLE = 'Ninja Trials: Trial of the Stones — ' + BasePageContext.TITLE
+	URL = BasePageContext.URL + '/trial-of-the-stones'
+	BUTTON = "Answer"
+	ANOTHER_WORD_FOR_STONE = 'rock'
+	PASSWORD = 'bamboo'
+	SUCCESS = 'Success!'
+	JESSICA_TOTAL_WEALTH = 3000
+	BERNARD_TOTAL_WEALTH = 2000
+	CHECK_ANSWERS = 'Trial Complete'
+
