1c6d1868e56933bccb72bc5120226d2b404f11e3
Egor Kostan <igorkostan@gmail.com>
2019-07-28 16:58:16 -0700

    Create trial_stones_page_locator.py

---
+++ professional_browser_automation/page_locators/trial_stones_page_locator.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.common.by import By
+
+
+class TrialStonesPageLocator:
+
+    TITLE = (By.TAG_NAME, 'title')
+
+    RIDDLE_INPUT_FIELD = (By.ID, 'r1Input')
+    RIDDLE_ANSWER_BUTTON = (By.ID, 'r1Btn')
+
+    PASSWORD_INPUT_FIELD = (By.ID, 'r2Input')
+    PASSWORD_ANSWER_BUTTON = (By.ID, 'r2Butn')
+    PASSWORD = (By.XPATH, '//*[@id="passwordBanner"]/h4')
+    PASSWORD_SUCCESS = (By.XPATH, '//*[@id="successBanner1"]/h4')
+
+    JESSICA_TOTAL_WEALTH = (By.XPATH, '//b[text()=\'Jessica\']/../../p')
+    JESSICA = (By.XPATH, '//b[text()=\'Jessica\']')
+
+    BERNARD_TOTAL_WEALTH = (By.XPATH, '//b[text()=\'Bernard\']/../../p')
+    BERNARD = (By.XPATH, '//b[text()=\'Bernard\']')
+
+    RICHEST_MERCHANT_FIELD = (By.ID, 'r3Input')
+    RICHEST_MERCHANT_BUTTON = (By.ID, 'r3Butn')
+    RICHEST_MERCHANT_SUCCESS = (By.XPATH, '//*[@id="successBanner2"]/h4')
+
+    CHECK_ANSWERS_BUTTON = (By.ID, 'checkButn')
+    TRIAL_COMPLETE = (By.XPATH, '//*[@id="trialCompleteBanner"]/h4')
+
