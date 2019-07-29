00c78f9b5635a2c2fca76f7bde8d7a74cf21bbf0
Egor Kostan <igorkostan@gmail.com>
2019-07-28 21:40:58 -0700

    Update frames_handling_test.py

--- professional_browser_automation/tests/frames_handling_test.py
+++ professional_browser_automation/tests/frames_handling_test.py
@@ -1,9 +1,28 @@
 import unittest
+import selenium
+from professional_browser_automation.page_models.iframe_training_model import IFrameTrainingModel
+from professional_browser_automation.page_context.iframe_training_context import IFrameTrainingContext
 
 
 class FramesHandlingTestCase(unittest.TestCase):
-	def test_something(self):
+
+	@classmethod
+	def setUpClass(cls):
+		cls.page = IFrameTrainingModel(selenium.webdriver.Chrome())
+		cls.page.go()
+
+	def test_url(self):
+		self.assertEqual(IFrameTrainingContext.URL, self.page.page_url)
+
+	def test_title(self):
+		self.assertEqual(IFrameTrainingContext.TITLE, self.page.title)
+
+	def test_switch_to_frame(self):
 		pass
+
+	@classmethod
+	def tearDownClass(cls):
+		cls.page.quit()
 
 
 if __name__ == '__main__':
