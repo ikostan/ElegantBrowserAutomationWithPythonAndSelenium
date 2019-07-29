2ec14095b14a521f1250cdb45521f356d127bb4d
Egor Kostan <igorkostan@gmail.com>
2019-07-28 20:33:12 -0700

    Create iframe_training_context.py

---
+++ professional_browser_automation/page_context/iframe_training_context.py
@@ -0,0 +1,7 @@
+from professional_browser_automation.page_context.base_page_context import BasePageContext
+
+
+class IFrameTrainingContext(BasePageContext):
+
+	URL = BasePageContext.URL + '/iframe-training'
+	TITLE = 'IFrameTraining — ' + BasePageContext.TITLE
