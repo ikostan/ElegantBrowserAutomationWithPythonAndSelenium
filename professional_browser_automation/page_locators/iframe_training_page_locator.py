228e277074702781811b7d38b10e7da56eec8df9
Egor Kostan <igorkostan@gmail.com>
2019-07-28 22:19:36 -0700

    Update iframe_training_page_locator.py

--- professional_browser_automation/page_locators/iframe_training_page_locator.py
+++ professional_browser_automation/page_locators/iframe_training_page_locator.py
@@ -5,3 +5,4 @@ class IFrameTrainingPageLocator:
 
     TITLE = (By.TAG_NAME, 'title')
     IFRAME = (By.XPATH, '//*[@id="block-yui_3_17_2_1_1564338258424_5429"]/div/iframe')
+    PARAGRAPH = (By.XPATH, '//*[@id="block-5d3de848045889000188d389"]/div/p')
