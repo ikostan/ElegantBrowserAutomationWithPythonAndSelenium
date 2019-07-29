59b977c929271a56ea957280e53626f030667c2b
Egor Kostan <igorkostan@gmail.com>
2019-07-28 21:40:42 -0700

    Update base_element.py

--- professional_browser_automation/elements/base_element.py
+++ professional_browser_automation/elements/base_element.py
@@ -1,4 +1,3 @@
-import selenium
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from professional_browser_automation.elements.element import Element
@@ -11,8 +10,8 @@ class BaseElement(Element):
 		Wait until element becomes to be clickable and then click on it
 		:return:
 		'''
-		WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.locator))
-		self.element.click()
+		WebDriverWait(super().driver, 10).until(EC.element_to_be_clickable(super().locator))
+		super().element.click()
 		return None
 
 	@property
@@ -21,7 +20,7 @@ class BaseElement(Element):
 		Return text from web element
 		:return:
 		'''
-		return self.element.text
+		return super().element.text
 
 	@property
 	def value(self):
@@ -29,7 +28,7 @@ class BaseElement(Element):
 		Return text from web element
 		:return:
 		'''
-		return self.element.get_attribute('value')
+		return super().element.get_attribute('value')
 
 	@property
 	def is_visible(self):
@@ -37,4 +36,4 @@ class BaseElement(Element):
 		Returns 'is element visible' property value
 		:return:
 		'''
-		return self.element.is_displayed()
+		return super().element.is_displayed()
