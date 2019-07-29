a9858eb604e091c08f41cf7f32300595e5793162
Egor Kostan <igorkostan@gmail.com>
2019-07-28 20:44:57 -0700

    Update iframe_training_page_locator.py

--- professional_browser_automation/page_locators/iframe_training_page_locator.py
+++ professional_browser_automation/page_locators/iframe_training_page_locator.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-class TrialStonesPageLocator:
+class IFrameTrainingPageLocator:
 
     TITLE = (By.TAG_NAME, 'title')
     IFRAME = (By.XPATH, '//*[@id="block-yui_3_17_2_1_1564338258424_5429"]/div/iframe')
