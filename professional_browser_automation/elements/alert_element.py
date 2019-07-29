e187dda232e79dd345d988a69b47737250bf9249
Egor Kostan <igorkostan@gmail.com>
2019-07-28 21:56:38 -0700

    Update alert_element.py

--- professional_browser_automation/elements/alert_element.py
+++ professional_browser_automation/elements/alert_element.py
@@ -7,13 +7,29 @@ class AlertElement:
 
 	def __init__(self, driver: selenium.webdriver):
 
-		self.driver = driver
-		self.element = self._find()
+		self._driver = driver
+		self._element = self._find()
 
 	def _find(self):
 		'''
 		Returns element if located else raises TimeOut exception
 		:return:
 		'''
-		WebDriverWait(self.driver, 10).until(EC.alert_is_present())
-		return self.driver.switch_to.alert
+		WebDriverWait(self._driver, 10).until(EC.alert_is_present())
+		return self._driver.switch_to.alert
+
+	@property
+	def text(self):
+		'''
+		Returns text
+		:return:
+		'''
+		return self._element.text
+
+	def accept(self):
+		'''
+		Close alert
+		:return:
+		'''
+		self._element.accept()
+		return None
