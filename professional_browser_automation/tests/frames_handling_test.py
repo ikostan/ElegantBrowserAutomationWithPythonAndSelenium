323fc824594bc55e827b006ac63a6ca0e1d8e65b
Egor Kostan <igorkostan@gmail.com>
2019-07-28 22:19:29 -0700

    Update frames_handling_test.py

--- professional_browser_automation/tests/frames_handling_test.py
+++ professional_browser_automation/tests/frames_handling_test.py
@@ -1,7 +1,11 @@
 import unittest
 import selenium
+from selenium.common.exceptions import TimeoutException
+from professional_browser_automation.elements.base_element import BaseElement
+from professional_browser_automation.page_locators.training_ground_locator import TrainingGroundLocator
 from professional_browser_automation.page_models.iframe_training_model import IFrameTrainingModel
 from professional_browser_automation.page_context.iframe_training_context import IFrameTrainingContext
+from professional_browser_automation.page_context.training_ground_context import TrainingGroundContext
 
 
 class FramesHandlingTestCase(unittest.TestCase):
@@ -18,7 +22,25 @@ class FramesHandlingTestCase(unittest.TestCase):
 		self.assertEqual(IFrameTrainingContext.TITLE, self.page.title)
 
 	def test_switch_to_frame(self):
-		pass
+		# 1
+		self.assertEqual(IFrameTrainingContext.PARAGRAPH, self.page.paragraph)
+
+		# 2
+		self.page.switch_to_iframe()
+		with self.assertRaises(TimeoutException):
+			self.page.paragraph
+
+		# 3
+		button = BaseElement(self.page.driver, TrainingGroundLocator.BUTTON_1)
+		self.assertEqual(TrainingGroundContext.BUTTON_1, button.text)
+
+		# 4
+		self.page.switch_to_main_frame()
+		self.assertEqual(IFrameTrainingContext.PARAGRAPH, self.page.paragraph)
+
+		# 5
+		with self.assertRaises(TimeoutException):
+			button = BaseElement(self.page.driver, TrainingGroundLocator.BUTTON_1)
 
 	@classmethod
 	def tearDownClass(cls):
