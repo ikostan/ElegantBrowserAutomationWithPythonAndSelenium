79a1937fb246f49eeec0694d8a54e25e822dbbd6
Egor Kostan <igorkostan@gmail.com>
2019-07-28 20:28:05 -0700

    Update base_element.py

--- professional_browser_automation/elements/base_element.py
+++ professional_browser_automation/elements/base_element.py
@@ -1,22 +1,10 @@
 import selenium
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from professional_browser_automation.elements.element import Element
 
 
-class BaseElement:
-
-	def __init__(self, driver: selenium.webdriver, locator: tuple):
-
-		self.driver = driver
-		self.locator = locator
-		self.element = self._find()
-
-	def _find(self):
-		'''
-		Returns element if located else raises TimeOut exception
-		:return:
-		'''
-		return WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(self.locator))
+class BaseElement(Element):
 
 	def click(self):
 		'''
